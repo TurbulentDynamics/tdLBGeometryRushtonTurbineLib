f0c0a3646b2638dfa9fb85db3bb32cf31056e012
Andrey Galysh <galysh.andrey@gmail.com>
2021-07-28 18:03:46 +0300

    Add exclude geometry optional command line argument: -e, and also add -s <size> optional parameter to change size (default 500)

--- Sources/tdLBGeometryRushtonTurbineLibCPP/test-polar.cpp
+++ Sources/tdLBGeometryRushtonTurbineLibCPP/test-polar.cpp
@@ -18,9 +18,26 @@
 using tNi = long int;
 
 
-int main(){
-    
+int main(int argc, char *argv[]){
+
+    bool excludePointsParam = false;
+    bool readSize = false;
     tNi gridX = 500;
+    for (int i = 1; i < argc; i++) {
+        if (readSize) {
+            if (sscanf(argv[i], "%ld", &gridX) != 1) {
+                std::cerr << "-s has invalid parameter, needs integer" << std::endl;
+            }
+            readSize = false;
+        }
+        if (!strcmp(argv[i], "-e")) {
+            excludePointsParam = true;
+        } else if (!strcmp(argv[i], "-s")) {
+            readSize  = true;
+        } else {
+            std::cerr << argv[i] << " is unknown parameter" << std::endl;
+        }
+    }
     
     RushtonTurbine t = RushtonTurbine((int)gridX);
     
@@ -46,9 +63,26 @@ int main(){
     std::ofstream file;
     file.open("test--polar.ply");
 
+    bool *excludeGeomPoints = new bool[gridX*gridX*gridX];
+    if (excludePointsParam) {
+        for (auto v : gCPP.getFixedExcludePoints()) {
+            excludeGeomPoints[v.i*gridX*gridX + v.j*gridX + v.k] = true;
+        }
+    }
+
+    if (excludePointsParam) {
+        std::vector<PosPolar<tNi, float>> tmp;
+        for (auto&& p : geomCPP) {
+            if (!excludeGeomPoints[p.i*gridX*gridX + p.j*gridX + p.k]) {
+                tmp.push_back(p);
+            }
+        }
+        geomCPP = tmp;
+    }
+
     file << "ply\nformat ascii 1.0\nelement vertex " << geomCPP.size();
     file << "\nproperty int x\nproperty int y\nproperty int z\nend_header\n";
-    
+
     for (auto&& p : geomCPP){
         file << p.i <<" " << p.j << " " << p.k << "\n";
     }
